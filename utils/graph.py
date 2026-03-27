38294f7d5f63d04e5158371ee3f863d6aa02cbe6
Yamato Matsumura <yamtheyam104@gmail.com>
2026-03-27 16:07:01 -0600

    fixed issue where last x-tick wasn't showing on graph

--- utils/graph.py
+++ utils/graph.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 def draw_ticket_volumes(df):
-    # add monthly ticket volumes
+    # Add monthly ticket volumes
     df['Month'] = df['Created'].dt.to_period('M').apply(lambda r: r.start_time)
     monthly_counts = df.groupby('Month').size().reset_index(name="ticket_count")
 
@@ -13,6 +13,7 @@ def draw_ticket_volumes(df):
         end=df['Month'].max(),
         freq='MS'
     )
+
     # Reindex to include missing months
     monthly_counts = (
         monthly_counts.set_index('Month')
@@ -20,26 +21,43 @@ def draw_ticket_volumes(df):
         .rename_axis('Month')
         .reset_index()
     )
+
     # Plotly Chart
     fig = px.line(
         monthly_counts, x="Month", y="ticket_count",
         title="Monthly Ticket Volume", markers=True
     )
+
+    # Shade below line
     fig.update_traces(fill='tozeroy')
+
+    # Ensure last month's label is shown
+    x_start = monthly_counts['Month'].min()
+    x_end = monthly_counts['Month'].max() + pd.DateOffset(months=1)
     fig.update_layout(
         title_x=0.5,
         title_xanchor='center',
         title_font=dict(size=20),
-        paper_bgcolor='rgba(0,0,0,0)', # Transparent background
+        paper_bgcolor='rgba(0,0,0,0)',
         plot_bgcolor='rgba(0,0,0,0)',
         margin=dict(l=0, r=0, t=50, b=0),
         xaxis_title='Month',
         yaxis_title='Tickets',
         yaxis=dict(
-                range=[0, 130],
-                showgrid=True, 
-                gridcolor="#30363d", 
-                color="#8b949e"
+            range=[0, 130],
+            showgrid=True, 
+            gridcolor="#30363d", 
+            color="#8b949e"
+        ),
+        xaxis=dict(
+            range=[x_start, x_end],
+            showgrid=False,
+            color="#8b949e",
+            tickmode='linear',
+            dtick="M1",
+            tickformat="%b %Y",
+            tickangle=0, 
+            nticks=20
         ),
     )
     return fig
