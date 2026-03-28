7978101c98735ae5286fab2d172235d6fdf8443f
Yamato Matsumura <yamtheyam104@gmail.com>
2026-03-28 10:38:50 -0600

    rearranged files so overview page is a pge within directory

--- app.py
+++ app.py
@@ -1,37 +1,8 @@
 import streamlit as st
-import plotly.express as px
-import pandas as pd
 
-import utils.data as data
-import utils.visuals as visuals
+overview = st.Page("pages/overview.py", title="Ticket Overview", default=True)
+cd = st.Page("pages/classroom_down.py", title="Classroom Down Dashboard")
+crt = st.Page("pages/crt.py", title="CRT Dashboard")
 
-st.set_page_config(page_title="Ticket Overview", layout="wide")
-
-def main():
-    st.markdown("<h1 style='text-align: center; margin-bottom: 40px;'>Ticket Overview</h1>", unsafe_allow_html=True)
-
-    # Creating a structured layout
-    _, left, right, _ = st.columns([0.5, 4, 4, 0.5])
-
-    with left:
-        cd_df = data.load_cd_data()
-        cd_df = data.parse_cd_data(cd_df)
-
-        st.markdown("<h2 style='text-align: center; color: white; font-weight: 200; margin-bottom: 0px;'>Classroom Down</h2>", unsafe_allow_html=True)
-        
-        visuals.draw_ticket_count(cd_df)
-
-        st.plotly_chart(visuals.draw_ticket_volumes(cd_df), width='stretch')
-
-    with right:
-        crt_df = data.load_crt_data()
-        crt_df = data.parse_crt_data(crt_df)
-
-        st.markdown("<h2 style='text-align: center; color: white; font-weight: 200; margin-bottom: 0px;'>Classroom Response Team</h2>", unsafe_allow_html=True)
-
-        visuals.draw_ticket_count(crt_df)
-
-        st.plotly_chart(visuals.draw_ticket_volumes(crt_df), width='stretch')
-
-if __name__ == "__main__":
-    main()
+pg = st.navigation([overview, crt, cd])
+pg.run()
