3acc0f9440783a01a848d494a512924fbf2229c0
Yamato Matsumura <yamtheyam104@gmail.com>
2026-03-28 12:14:12 -0600

    removed cd page since not used for now

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
 overview = st.Page("pages/overview.py", title="Ticket Overview", default=True)
-cd = st.Page("pages/classroom_down.py", title="Classroom Down Dashboard")
+# cd = st.Page("pages/classroom_down.py", title="Classroom Down Dashboard")
 crt = st.Page("pages/crt.py", title="CRT Dashboard")
 
-pg = st.navigation([overview, crt, cd])
+# pg = st.navigation([overview, crt, cd])
+pg = st.navigation([overview, crt])
 pg.run()
